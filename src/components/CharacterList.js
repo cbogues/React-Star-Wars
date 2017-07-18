17bdbe7909e52f796daddcab9d405b787d335696
cbogues <YOUR-EMAIL-ADDRESS>
2017-07-18 07:20:46 -0700

    able to display character names but no info yet

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setCurrentCharacter } from '../reducer/character/actions';
 
-const CharacterList = ({ characters }) =>
+const CharacterList = ({ characters, setCharacter }) =>
 	<div id="character-list" className="col-md-6">
 		<h1>Characters</h1>
 		<ul>
-			{characters.map(c => 
-				<li key={c.name}>
+			{characters.map((c, i)=> 
+				<li onClick={setCharacter(i + 1)} key={c.name}>
 					{c.name}
 				</li>	
 			)}
 		</ul>	
-	</div>;
+	</div>
 
 const mapStateToProps = ({ characters }) => ({
-	characters,
+	characters
 });
 
-export default connect(mapStateToProps)(CharacterList);
+const mapDispatchToProps = dispatch => ({
+	setCharacter(id) {
+		return () => {
+			dispatch(setCurrentCharacter(id));
+		};
+	}
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CharacterList);
